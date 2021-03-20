ebd97da031a1b871a60d1e5a12081a7bd18fa6e4
Aina2bean <ritakura629@gmail.com>
2021-03-20 15:35:55 +0900

    取得したデータから配列を生成して、指定した電車が遅延しているかを判断する（検証用で特急サザンに指定）

--- js/train.js
+++ js/train.js
@@ -1,23 +1,24 @@
-let localTrain = '京王線'
+let localTrain = '特急サザン'
 
 let url = 'https://tetsudo.rti-giken.jp/free/delay.json';
 fetch(url)
 .then(response => response.json())
 .then(json => {
     console.log(json);
-    function ttt(){
+    function delayInfo(num){
+        let localData = [];
         for(let i = 0; i < json.length; i++) {
-            let stationList = json[i].name;
-            console.log(stationList);
-            return stationList;
+            localData.push(json[i].name);
         }
+        return localData;
     }
-    localData = ttt();
-    //if(stationList.indexOf(localTrain)) {
-        //console.log('あるよ');
-    //} else {
-        //console.log('ないよ');
-    //}
+    result = delayInfo();
+    console.log(result);
+    if(result.indexOf(localTrain) == -1 ) {
+        console.log(localTrain + 'は遅れてないよ');
+    } else {
+        console.log(localTrain + 'は遅れてるよ');
+    }
 });
 
 
